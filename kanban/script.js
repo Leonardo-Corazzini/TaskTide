9ec7c598c4136000ef03e17c94cac7b61e3fad0b
eduardorocco <eduardo_rocco@hotmail.it>
2024-11-13 12:41:03 +0100

    ADD function createEl and listeners

--- kanban/script.js
+++ kanban/script.js
@@ -3,6 +3,11 @@ const addBtn = document.querySelector('.add-button')
 const overlayContainer = document.querySelector('.overlay-container')
 const addForm = document.getElementById('add-form')
 // console.log(overlayContainer,addForm);
+const toDoText = document.getElementById('to-do-text')
+const cardBody = document.querySelector('.card-body')
+
+
+
 addBtn.addEventListener('click', function(event) {
     overlayContainer.classList.remove('d-off')
     overlayContainer.classList.add('overflow-h')
@@ -10,3 +15,43 @@ addBtn.addEventListener('click', function(event) {
 })
 
 
+addForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    overlayContainer.classList.add('d-off')
+    overlayContainer.classList.remove('overflow-h')
+    const toDoEl = createCard('div', ['to-do-element'], [], el=>(el.innerText = toDoText.value))
+    cardBody.appendChild(toDoEl)
+
+    
+})
+
+
+
+
+function createCard(
+    tagName,
+    classList = [],
+    content = [],
+    callback = false
+) {
+    const el = document.createElement(tagName);
+
+    if (classList.length > 0) {
+        el.classList.add(...classList);
+    }
+
+
+    if (callback) {
+        callback(el);
+    }
+    if (Array.isArray(content)) {
+        for (let i = 0; i < content.length; i++) {
+            if (content[i] instanceof HTMLElement) {
+                el.appendChild(content[i]);
+            }
+        }
+    } else {
+        console.error("Non posso aggiungere l'elemento");
+    }
+    return el;
+}
